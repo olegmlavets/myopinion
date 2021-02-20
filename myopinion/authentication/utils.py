a2dbbed158ccf60069cb1295dd6ff950b482f5f1
Oleg Mlavets <mlavetso@gmail.com>
2021-02-20 20:40:34 +0200

    add threading to email send

--- myopinion/authentication/utils.py
+++ myopinion/authentication/utils.py
@@ -1,9 +1,18 @@
 from django.core.mail import EmailMessage
+import threading
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self) -> None:
+        self.email.send()
 
 
 class Util:
     @staticmethod
-    def send_email(data: dict) -> bool:
+    def send_email(data: dict) -> None:
         email = EmailMessage(**data)  # param "to" must be a tuple or list
-        status = email.send()
-        return status
+        EmailThread(email).start()
